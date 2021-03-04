94f097a253ac98c48169e03b13c65c00abee9418
Elvar Laxdal <elvarlax@gmail.com>
2021-03-04 15:45:28 +0100

    Cleanup

--- Databases/FamilyDB.sql
+++ Databases/FamilyDB.sql
@@ -1,9 +1,13 @@
 CREATE DATABASE IF NOT EXISTS familyDB;
 USE familyDB;
 
-CREATE TABLE family(
-   PersonName VARCHAR(40) NOT NULL,
-   Birthday int,
-   PRIMARY KEY (PersonName));
-   INSERT INTO family VALUES ('Henry', 19591006);
-   INSERT INTO family VALUES ('Lilly', 19940224);
+CREATE TABLE family
+(
+    PersonName VARCHAR(40) NOT NULL,
+    Birthday   int,
+    PRIMARY KEY (PersonName)
+);
+
+INSERT INTO family
+VALUES ('Henry', 19591006),
+       ('Lilly', 19940224);
