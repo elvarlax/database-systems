fbd25ad15945aa06d08b419062777d0a84f9d88e
Elvar Laxdal <elvarlax@gmail.com>
2021-05-17 13:54:03 +0200

    Forgot guest foreign key

--- Exams/2020/HotelDatabase.sql
+++ Exams/2020/HotelDatabase.sql
@@ -8,11 +8,22 @@ CREATE TABLE Hotel (
     PRIMARY KEY (hotelNo)
 );
 
-INSERT Hotel (hotelNo, hotelName, city) VALUES ('H002', 'Grand Hotel', 'London');
-INSERT Hotel (hotelNo, hotelName, city) VALUES ('H003', 'Sanslits', 'Paris');
-INSERT Hotel (hotelNo, hotelName, city) VALUES ('H005', 'City Hotel', 'London');
-INSERT Hotel (hotelNo, hotelName, city) VALUES ('H011', 'Palads', 'Copenhagen');
-INSERT Hotel (hotelNo, hotelName, city) VALUES ('H012', 'Piazza Hotel', 'Rome');
+INSERT INTO Hotel VALUES ('H002', 'Grand Hotel', 'London');
+INSERT INTO Hotel VALUES ('H003', 'Sanslits', 'Paris');
+INSERT INTO Hotel VALUES ('H005', 'City Hotel', 'London');
+INSERT INTO Hotel VALUES ('H011', 'Palads', 'Copenhagen');
+INSERT INTO Hotel VALUES ('H012', 'Piazza Hotel', 'Rome');
+
+CREATE TABLE Guest (
+    guestNo VARCHAR(4),
+    guestName VARCHAR(15),
+    guestAddress VARCHAR(25),
+    PRIMARY KEY (guestNo)
+);
+
+INSERT INTO Guest VALUES ('G221', 'Peter Schmidt', 'Lake Str. 5, Holte');
+INSERT INTO Guest VALUES ('G324', 'Anders Jensen', 'High Str. 1, Lyngby');
+INSERT INTO Guest VALUES ('G329', 'Mary Hanson', 'Castle Blv. 2, Sorgenfri');
 
 CREATE TABLE Room (
     hotelNo VARCHAR(4),
@@ -23,26 +34,15 @@ CREATE TABLE Room (
     FOREIGN KEY (hotelNo) REFERENCES Hotel(hotelNo)
 );
 
-INSERT Room (hotelNo, roomNo, roomType, price) VALUES ('H002', '113', 'Family', 80);
-INSERT Room (hotelNo, roomNo, roomType, price) VALUES ('H002', '200', 'Double', 50);
-INSERT Room (hotelNo, roomNo, roomType, price) VALUES ('H005', '214', 'Double', 100);
-INSERT Room (hotelNo, roomNo, roomType, price) VALUES ('H005', '215', 'Single', 75);
-INSERT Room (hotelNo, roomNo, roomType, price) VALUES ('H005', '216', 'Family', 120);
-INSERT Room (hotelNo, roomNo, roomType, price) VALUES ('H011', '214', 'Double', 90);
-INSERT Room (hotelNo, roomNo, roomType, price) VALUES ('H011', '215', 'Double', 90);
-INSERT Room (hotelNo, roomNo, roomType, price) VALUES ('H011', '216', 'Single', 89);
-INSERT Room (hotelNo, roomNo, roomType, price) VALUES ('H012', '101', 'Single', 83);
-
-CREATE TABLE Guest (
-    guestNo VARCHAR(4),
-    guestName VARCHAR(15),
-    guestAddress VARCHAR(25),
-    PRIMARY KEY (guestNo)
-);
-
-INSERT Guest (guestNo, guestName, guestAddress) VALUES ('G221', 'Peter Schmidt', 'Lake Str. 5, Holte');
-INSERT Guest (guestNo, guestName, guestAddress) VALUES ('G324', 'Anders Jensen', 'High Str. 1, Lyngby');
-INSERT Guest (guestNo, guestName, guestAddress) VALUES ('G329', 'Mary Hanson', 'Castle Blv. 2, Sorgenfri');
+INSERT INTO Room VALUES ('H002', '113', 'Family', 80);
+INSERT INTO Room VALUES ('H002', '200', 'Double', 50);
+INSERT INTO Room VALUES ('H005', '214', 'Double', 100);
+INSERT INTO Room VALUES ('H005', '215', 'Single', 75);
+INSERT INTO Room VALUES ('H005', '216', 'Family', 120);
+INSERT INTO Room VALUES ('H011', '214', 'Double', 90);
+INSERT INTO Room VALUES ('H011', '215', 'Double', 90);
+INSERT INTO Room VALUES ('H011', '216', 'Single', 89);
+INSERT INTO Room VALUES ('H012', '101', 'Single', 83);
 
 CREATE TABLE Booking (
     hotelNo VARCHAR(4),
@@ -50,14 +50,15 @@ CREATE TABLE Booking (
     night DATE,
     guestNo VARCHAR(4),
     isPaid BOOLEAN,
-    FOREIGN KEY (hotelNo, roomNo) REFERENCES Room(hotelNo, roomNo)
+    FOREIGN KEY (hotelNo, roomNo) REFERENCES Room(hotelNo, roomNo),
+	FOREIGN KEY (guestNo) REFERENCES Guest(guestNo)
 );
 
-INSERT Booking (hotelNo, roomNo, night, guestNo, isPaid) VALUES ('H002', '113', '2021-06-01', 'G324', 0);
-INSERT Booking (hotelNo, roomNo, night, guestNo, isPaid) VALUES ('H002', '113', '2021-06-02', 'G324', 0);
-INSERT Booking (hotelNo, roomNo, night, guestNo, isPaid) VALUES ('H005', '214', '2021-02-02', 'G324', 1);
-INSERT Booking (hotelNo, roomNo, night, guestNo, isPaid) VALUES ('H005', '214', '2021-03-12', 'G324', 0);
-INSERT Booking (hotelNo, roomNo, night, guestNo, isPaid) VALUES ('H005', '216', '2021-07-07', 'G221', 0);
-INSERT Booking (hotelNo, roomNo, night, guestNo, isPaid) VALUES ('H011', '214', '2021-04-22', 'G324', 1);
-INSERT Booking (hotelNo, roomNo, night, guestNo, isPaid) VALUES ('H011', '214', '2021-08-10', 'G221', 1);
-INSERT Booking (hotelNo, roomNo, night, guestNo, isPaid) VALUES ('H011', '215', '2021-08-10', 'G221', 1);
+INSERT INTO Booking VALUES ('H002', '113', '2021-06-01', 'G324', 0);
+INSERT INTO Booking VALUES ('H002', '113', '2021-06-02', 'G324', 0);
+INSERT INTO Booking VALUES ('H005', '214', '2021-02-02', 'G324', 1);
+INSERT INTO Booking VALUES ('H005', '214', '2021-03-12', 'G324', 0);
+INSERT INTO Booking VALUES ('H005', '216', '2021-07-07', 'G221', 0);
+INSERT INTO Booking VALUES ('H011', '214', '2021-04-22', 'G324', 1);
+INSERT INTO Booking VALUES ('H011', '214', '2021-08-10', 'G221', 1);
+INSERT INTO Booking VALUES ('H011', '215', '2021-08-10', 'G221', 1);
