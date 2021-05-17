ccdb3fbb8b16de53f132d2b2b0ad89ab031c4d2d
Elvar Laxdal <elvarlax@gmail.com>
2021-05-17 15:22:46 +0200

    Adding database 2019

---
+++ Exams/2019/ResearchDatabase.sql
@@ -0,0 +1,78 @@
+CREATE DATABASE ResearchDatabase;
+USE ResearchDatabase;
+
+CREATE TABLE Institution (
+    iname VARCHAR(5),
+    country VARCHAR(2),
+    PRIMARY KEY (iname)
+);
+
+INSERT INTO Institution VALUES 
+('DTU', 'DK'), 
+('Uni1', 'US'), 
+('USUni', 'US'), 
+('SUni', 'E');
+
+CREATE TABLE Researcher (
+    rid INT,
+    rname VARCHAR(5),
+    iname VARCHAR(5),
+    PRIMARY KEY (rid)
+);
+
+INSERT INTO Researcher VALUES 
+(1, 'Anne', 'DTU'), 
+(2, 'Peter', 'Uni1'), 
+(3, 'Signe', 'DTU'), 
+(4, 'Jan', 'USUni');
+
+CREATE TABLE Article (
+    aid INT,
+    aname VARCHAR(30),
+	year YEAR,
+    PRIMARY KEY (aid)
+);
+
+INSERT INTO Article VALUES 
+(1, 'Modern Database Systems', 2019), 
+(2, 'Formal Methods in Practice', 2018), 
+(3, 'Tools of the Future', 2018);
+
+CREATE TABLE Authors (
+    aid INT,
+    rid INT,
+	PRIMARY KEY (aid, rid),
+    FOREIGN KEY (aid) REFERENCES Article(aid),
+    FOREIGN KEY (rid) REFERENCES Researcher(rid)
+);
+
+INSERT INTO Authors VALUES 
+(1, 1), 
+(2, 1), 
+(2, 3), 
+(3, 4);
+
+CREATE TABLE Project (
+    pname VARCHAR(12),
+    iname VARCHAR(5),
+	PRIMARY KEY (pname)
+);
+
+INSERT INTO Project VALUES 
+('NextDB', 'DTU'), 
+('RobustRailS', 'DTU'), 
+('AIfuture', 'USUni');
+
+CREATE TABLE Participates (
+    pname VARCHAR(12),
+    rid INT,
+    PRIMARY KEY (pname, rid),
+    FOREIGN KEY (pname) REFERENCES Project(pname),
+    FOREIGN KEY (rid) REFERENCES Researcher(rid)
+);
+
+INSERT INTO Participates VALUES 
+('AIfuture', 1), 
+('AIfuture', 4), 
+('RobustRailS', 1), 
+('RobustRailS', 2);
--- Exams/2020/HotelDatabase.sql
+++ Exams/2020/HotelDatabase.sql
@@ -8,24 +8,26 @@ CREATE TABLE Hotel (
     PRIMARY KEY (hotelNo)
 );
 
-INSERT INTO Hotel VALUES ('H002', 'Grand Hotel', 'London');
-INSERT INTO Hotel VALUES ('H003', 'Sanslits', 'Paris');
-INSERT INTO Hotel VALUES ('H005', 'City Hotel', 'London');
-INSERT INTO Hotel VALUES ('H011', 'Palads', 'Copenhagen');
-INSERT INTO Hotel VALUES ('H012', 'Piazza Hotel', 'Rome');
+INSERT INTO Hotel VALUES 
+('H002', 'Grand Hotel', 'London'), 
+('H003', 'Sanslits', 'Paris'), 
+('H005', 'City Hotel', 'London'), 
+('H011', 'Palads', 'Copenhagen'), 
+('H012', 'Piazza Hotel', 'Rome');
 
-CREATE TABLE Hotel (
+CREATE TABLE Guest (
     guestNo VARCHAR(4),
     guestName VARCHAR(15),
     guestAddress VARCHAR(25),
     PRIMARY KEY (guestNo)
 );
 
-INSERT INTO Guest VALUES ('G221', 'Peter Schmidt', 'Lake Str. 5, Holte');
-INSERT INTO Guest VALUES ('G324', 'Anders Jensen', 'High Str. 1, Lyngby');
-INSERT INTO Guest VALUES ('G329', 'Mary Hanson', 'Castle Blv. 2, Sorgenfri');
+INSERT INTO Guest VALUES 
+('G221', 'Peter Schmidt', 'Lake Str. 5, Holte'), 
+('G324', 'Anders Jensen', 'High Str. 1, Lyngby'), 
+('G329', 'Mary Hanson', 'Castle Blv. 2, Sorgenfri');
 
-CREATE TABLE Hotel (
+CREATE TABLE Room (
     hotelNo VARCHAR(4),
     roomNo VARCHAR(3),
     roomType ENUM('Single', 'Double', 'Family'),
@@ -34,17 +36,18 @@ CREATE TABLE Hotel (
     FOREIGN KEY (hotelNo) REFERENCES Hotel(hotelNo)
 );
 
-INSERT INTO Room VALUES ('H002', '113', 'Family', 80);
-INSERT INTO Room VALUES ('H002', '200', 'Double', 50);
-INSERT INTO Room VALUES ('H005', '214', 'Double', 100);
-INSERT INTO Room VALUES ('H005', '215', 'Single', 75);
-INSERT INTO Room VALUES ('H005', '216', 'Family', 120);
-INSERT INTO Room VALUES ('H011', '214', 'Double', 90);
-INSERT INTO Room VALUES ('H011', '215', 'Double', 90);
-INSERT INTO Room VALUES ('H011', '216', 'Single', 89);
-INSERT INTO Room VALUES ('H012', '101', 'Single', 83);
+INSERT INTO Room VALUES 
+('H002', '113', 'Family', 80), 
+('H002', '200', 'Double', 50), 
+('H005', '214', 'Double', 100), 
+('H005', '215', 'Single', 75), 
+('H005', '216', 'Family', 120), 
+('H011', '214', 'Double', 90), 
+('H011', '215', 'Double', 90), 
+('H011', '216', 'Single', 89), 
+('H012', '101', 'Single', 83);
 
-CREATE TABLE Hotel (
+CREATE TABLE Booking (
     hotelNo VARCHAR(4),
     roomNo VARCHAR(3),
     night DATE,
@@ -54,11 +57,12 @@ CREATE TABLE Hotel (
     FOREIGN KEY (guestNo) REFERENCES Guest(guestNo)
 );
 
-INSERT INTO Booking VALUES ('H002', '113', '2021-06-01', 'G324', 0);
-INSERT INTO Booking VALUES ('H002', '113', '2021-06-02', 'G324', 0);
-INSERT INTO Booking VALUES ('H005', '214', '2021-02-02', 'G324', 1);
-INSERT INTO Booking VALUES ('H005', '214', '2021-03-12', 'G324', 0);
-INSERT INTO Booking VALUES ('H005', '216', '2021-07-07', 'G221', 0);
-INSERT INTO Booking VALUES ('H011', '214', '2021-04-22', 'G324', 1);
-INSERT INTO Booking VALUES ('H011', '214', '2021-08-10', 'G221', 1);
-INSERT INTO Booking VALUES ('H011', '215', '2021-08-10', 'G221', 1);
+INSERT INTO Booking VALUES 
+('H002', '113', '2021-06-01', 'G324', 0),
+('H002', '113', '2021-06-02', 'G324', 0),
+('H005', '214', '2021-02-02', 'G324', 1),
+('H005', '214', '2021-03-12', 'G324', 0),
+('H005', '216', '2021-07-07', 'G221', 0),
+('H011', '214', '2021-04-22', 'G324', 1),
+('H011', '214', '2021-08-10', 'G221', 1),
+('H011', '215', '2021-08-10', 'G221', 1);
